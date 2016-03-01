f3115f53ff4710c26f2bac0142a1a6311fea85b3
Anders Bjerner <abjerner@skybrud.dk>
2016-03-01 14:04:07 +0100

    Added extension method for removing tabs from a list

---
+++ src/Skybrud.Umbraco.Dashboard/Extensions/DashboardTabListExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Skybrud.Umbraco.Dashboard.Model;
+
+namespace Skybrud.Umbraco.Dashboard.Extensions {
+    
+    /// <summary>
+    /// Various extension methods for <see cref="List&lt;DashboardTab&gt;"/>
+    /// </summary>
+    public static class DashboardTabListExtensions {
+        
+        /// <summary>
+        /// Removes all tabs matching the specified <code>predicate</code>.
+        /// </summary>
+        /// <param name="list">The list of tabs.</param>
+        /// <param name="predicate">The predicate used for matching the tabs.</param>
+        public static void Remove(this List<DashboardTab> list, Func<DashboardTab, bool> predicate) {
+            foreach (DashboardTab item in list.ToArray()) {
+                if (predicate(item)) {
+                    list.Remove(item);
+                }
+            }
+        }
+
+    }
+
+}
